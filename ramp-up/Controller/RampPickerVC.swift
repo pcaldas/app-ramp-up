15a94fe77131a1b81a815319ec0af9ccc8de6c04
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-28 17:31:30 -0300

    create the popoverView

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,36 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by Paulo Caldas on 28/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    }
+
+}
